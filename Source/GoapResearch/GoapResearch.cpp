72214ef8ea9eae3e401bb0944f122e604c1d7457
Yannick Godeau <39457052+Yeannick@users.noreply.github.com>
2022-01-10 17:14:55 +0100

    clear repo

--- Source/GoapResearch/GoapResearch.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GoapResearch.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GoapResearch, "GoapResearch" );
